5dd4212c7b19c8be5f892d89afb89e5171415aa2
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-04-30 16:48:03 +0100

    Initial commit, sample mqtt client

---
+++ client/mqtt_client_test.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+
+counter = 0
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global counter
+    counter = counter + 1
+    print("{} {}: {}".format(counter, msg.topic, msg.payload.decode("utf-8") ))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+host = "dangilsystem.zapto.org"
+
+client.connect(host, 1883, 60)
+
+client.subscribe("work/precache")
+print("Subscribed")
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
