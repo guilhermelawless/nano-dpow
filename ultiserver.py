d7c8f991511be4e2e887e3cfed291d57dbc0fac3
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-05-18 09:03:56 +0100

    Use connection pool for redis, other fixes and restructuring

--- ultiserver.py
+++ ultiserver.py
@@ -1,98 +1,125 @@
+#!/usr/bin/env python3
+
+from functools import wraps
 import asyncio
 from aiohttp import web
 import aiomqtt
 import time
 import aioredis
-import fourletterphat
 
-#mqtt_client = aiomqtt.Client(client_id="nanotest", clean_session=True, userdata=None,transport="tcp")
+
+def display_wrapper():
+    return None
+    try:
+        import fourletterphat
+        obj = fourletterphat
+    except ImportError as e:
+        print(e)
+        obj = None
+    except PermissionError as e:
+        print(e)
+        obj = None
+
+    return obj
+
+
 #host = "dangilsystem.zapto.org"
+display = display_wrapper()
 host = "localhost"
+redis_server = "redis://localhost"
+loop = asyncio.get_event_loop()
 
 
-@asyncio.coroutine
-async def send_mqtt(topic, message):
-    mqttc = aiomqtt.Client(asyncio.get_event_loop(), "nanotest")
-    await mqttc.connect(host, port=1883, keepalive=10)
 
-    mqttc.publish(topic, message)
-        
-    mqttc.disconnect()
+class DpowServer(object):
 
-async def redis_insert(key, value):
-    loop = asyncio.get_event_loop()
-    redis = await aioredis.create_redis('redis://localhost', loop=loop)
-    await redis.set(key, value )
-    redis.close()
-    await redis.wait_closed()
+    def __init__(self):
+        self.redis_pool = aioredis.create_pool(
+            redis_server,
+            minsize=5, maxsize=15,
+            loop=loop)
 
-async def redis_delete(key):
-    loop = asyncio.get_event_loop()
-    redis = await aioredis.create_redis('redis://localhost', loop=loop)
-    outcome = await redis.delete(str(key))
-    redis.close()
-    await redis.wait_closed()
-    print("Delete: {} {}".format(outcome, key))
+    async def wait_init(self):
+        await self.redis_pool
 
-async def redis_getkey(key):
-    loop = asyncio.get_event_loop()
-    redis = await aioredis.create_redis('redis://localhost', loop=loop)
-    val = await redis.get(key)
-    redis.close()
-    await redis.wait_closed()
-    return val.decode("utf-8") 
+    def __del__(self):
+        self.redis_pool.close()
+        #TODO: how to wait_close() ?
 
-async def redis_exists(key):
-    loop = asyncio.get_event_loop()
-    redis = await aioredis.create_redis('redis://localhost', loop=loop)
-    exists = await redis.exists(key)
-    redis.close()
-    await redis.wait_closed()
-    return exists
+    async def redis_insert(self, key: str, value: str):
+        await self.redis_pool.execute('SET', key, value )
 
-async def handle(request):
-    data = await request.json()
-    account_exists = await redis_exists(data['account'])
-    if account_exists == 1:
-        fourletterphat.set_decimal(1,True)
+    async def redis_delete(self, key: str):
+        outcome = await self.redis_pool.execute('DELETE', key)
+        print("Delete: {} {}".format(outcome, key))
 
-        print("Old account: {}".format(data['account']))
-        account_hash = await redis_getkey(data['account'])
-        print("Account Hash: {} {}".format(account_hash, data['hash']))
-        if account_hash != data['hash']:
-            print("New Hash, updating")
-            await redis_insert(data['account'], data['hash'] )
+    async def redis_getkey(self, key: str):
+        val = await self.redis_pool.execute('GET', key)
+        return val.decode("utf-8")
 
-            await redis_delete(account_hash)
-            print("Deleted")
-            await redis_insert(data['hash'] , "0")
-            print("New Entry Inserted")
-            await send_mqtt("work/precache", data['hash'])
+    async def redis_exists(self, key: str):
+        exists = await redis.execute('EXISTS', key)
+        return exists
+
+    @asyncio.coroutine
+    async def send_mqtt(topic, message):
+        mqttc = aiomqtt.Client(asyncio.get_event_loop(), "nanotest")
+        await mqttc.connect(host, port=1883, keepalive=10)
+        mqttc.publish(topic, message)
+        mqttc.disconnect()
+
+    def on_connect(client, userdata, flags, rc):
+        print("Connected to Rx")
+        client.subscribe("work/precache")
+
+
+    async def post_handle(self, request):
+        data = await request.json()
+        account_exists = await self.redis_exists(data['account'])
+        if account_exists == 1:
+            if display: display.set_decimal(1,True)
+
+            print("Old account: {}".format(data['account']))
+            frontier = await self.redis_getkey(data['account'])
+            print("Account Frontier: {} {}".format(frontier, data['hash']))
+            if frontier != data['hash']:
+                print("New Hash, updating")
+                await asyncio.gather(
+                    self.redis_insert(data['account'], data['hash']),
+                    self.redis_delete(frontier),
+                    self.redis_insert(data['hash'] , "0")
+                )
+                print("Deleted")
+                print("New Entry Inserted")
+                await self.send_mqtt("work/precache", data['hash'])
+            else:
+                print("Duplicate")
+
+            if display: display.set_decimal(0,False)
+
         else:
-            print("Duplicate")
+            if display: display.set_decimal(0,True)
 
-        fourletterphat.set_decimal(0,False)
+            print("New account: {}".format(data['account']))
+            await asyncio.gather(
+                self.redis_insert(data['account'], data['hash']),
+                self.redis_insert(data['hash'], "0"),
+                self.send_mqtt("work/precache", data['hash'])
+            )
+            print("Inserted")
 
-    else:
-        fourletterphat.set_decimal(0,True)
+            if display: display.set_decimal(0,False)
 
-        print("New account: {}".format(data['account']))
-        await redis_insert(data['account'], data['hash'] )
-        await redis_insert(data['hash'], "0")
-        print("Inserted")
-        await send_mqtt("work/precache", data['hash'])
+        return web.Response(text="test")
 
-        fourletterphat.set_decimal(0,False)
+if display:
+    display.print_str('dPoW')
+    display.show()
 
-    return web.Response(text="test")
+server = DpowServer()
+loop.run_until_complete(server.wait_init())
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected to Rx")
-    client.subscribe("work/precache")
-
-fourletterphat.print_str('dPoW')
-fourletterphat.show()
-app = web.Application()
-app.router.add_post('/', handle)
+app = web.Application(loop=loop)
+app.router.add_post('/', server.post_handle)
 
 web.run_app(app, port=5030)
