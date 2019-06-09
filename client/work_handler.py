faa06165d64f0a02ee93fa8a76ccfd4361fc2f58
Guilherme Lawless <guilawless@live.com>
2019-06-09 19:35:46 +0100

    Catch exceptions in queue work_cancel

--- client/work_handler.py
+++ client/work_handler.py
@@ -1,3 +1,4 @@
+import asyncio
 import requests
 import aiohttp
 import aiohttp_requests
@@ -28,12 +29,17 @@ class WorkHandler(object):
         return block_hash in self.work_queue
 
     async def queue_cancel(self, block_hash: str):
-        # Remove before cancelling as this means someone has completed this work already
-        self.work_queue.remove(block_hash)
-        await self.session.post(self.worker_uri, json={
-            "action": "work_cancel",
-            "hash": block_hash
-        })
+        try:
+            self.work_queue.remove(block_hash)
+            await self.session.post(self.worker_uri, json={
+                "action": "work_cancel",
+                "hash": block_hash
+            })
+        except KeyError:
+            pass
+        except Exception as e:
+            print(f"Work handler error: {e}")
+
 
     async def queue_work(self, work_type: str, block_hash: str, difficulty: str):
         try:
