6d067eea4025aead9eb75e00d0b7987696202b3e
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-06-09 13:28:53 +0100

    Update IP for service hash request script

--- service/random_hash_request.py
+++ service/random_hash_request.py
@@ -15,6 +15,6 @@ else:
     start_time = time.time()
     json_request = {"hash" : hash, "account": "xrb_1dpowseruicetest111111111111111111111111111111111111ptmcz6f9", "user": user, "api_key" : key, 'timeout': 1}
     print(json_request)
-    r = requests.post('http://127.0.0.1:5030/service/', json = json_request)
+    r = requests.post('http://139.59.134.66:5030/service/', json = json_request)
     complete_time = time.time()
     print(r.text + "\nTook: " + str(complete_time - start_time))
