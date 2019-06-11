f9f0ed9e157e412f0b66fb9b34faef58c54b48a7
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-06-11 19:27:44 +0000

    Improved logging

--- server/dpow/logger.py
+++ server/dpow/logger.py
@@ -1,19 +1,21 @@
 import sys
 import logging
+from logging.handlers import WatchedFileHandler, TimedRotatingFileHandler
 
 def get_logger():
-	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s - %(message)s')
-	logger = logging.getLogger("dpow")
-	logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s - %(message)s", "%Y-%m-%d %H:%M:%S %z")
+    logger = logging.getLogger("dpow")
+    logger.setLevel(logging.DEBUG)
 
-	handler = logging.StreamHandler(sys.stderr)
-	handler.setLevel(logging.WARN)
-	handler.setFormatter(formatter)
-	logger.addHandler(handler)
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setLevel(logging.WARN)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-	filehandler = logging.FileHandler("log.txt", 'a', 'utf-8')
-	filehandler.setLevel(logging.DEBUG)
-	filehandler.setFormatter(formatter)
-	logger.addHandler(filehandler)
-
-	return logger
+    log_file = "/tmp/dpow.txt"
+    filehandler = WatchedFileHandler(log_file)
+    filehandler.setLevel(logging.DEBUG)
+    filehandler.setFormatter(formatter)
+    logger.addHandler(filehandler)
+    logger.addHandler(TimedRotatingFileHandler(log_file, when="d", interval=1, backupCount=100))
+    return logger
