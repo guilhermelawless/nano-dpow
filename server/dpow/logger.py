f71c790f94e4f876af78aa190d1245e6abd8e357
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-06-12 14:35:27 +0000

    Fix duplicate message logging, remove stream handler

--- server/dpow/logger.py
+++ server/dpow/logger.py
@@ -3,19 +3,12 @@ import logging
 from logging.handlers import WatchedFileHandler, TimedRotatingFileHandler
 
 def get_logger():
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s - %(message)s", "%Y-%m-%d %H:%M:%S %z")
     logger = logging.getLogger("dpow")
-    logger.setLevel(logging.DEBUG)
-
-    handler = logging.StreamHandler(sys.stderr)
-    handler.setLevel(logging.WARN)
+    logging.basicConfig(level=logging.DEBUG)
+    log_file = "/tmp/dpow.txt"
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s", "%Y-%m-%d %H:%M:%S %z")
+    handler = WatchedFileHandler(log_file)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-
-    log_file = "/tmp/dpow.txt"
-    filehandler = WatchedFileHandler(log_file)
-    filehandler.setLevel(logging.DEBUG)
-    filehandler.setFormatter(formatter)
-    logger.addHandler(filehandler)
     logger.addHandler(TimedRotatingFileHandler(log_file, when="d", interval=1, backupCount=100))
     return logger
