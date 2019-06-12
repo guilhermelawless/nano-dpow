cc38019c2b83fecdd10d0900ade9e2782ac2f1a5
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-06-12 22:21:14 +0100

    Use https for service requests

--- service/random_hash_request.py
+++ service/random_hash_request.py
@@ -16,6 +16,6 @@ else:
     json_request = {"hash" : hash, "account": "xrb_1dpowseruicetest111111111111111111111111111111111111ptmcz6f9", "user": user, "api_key" : key, "timeout" : 30}
     # json_request = {"hash" : hash, "user": user, "api_key" : key, "timeout" : 30, "difficulty": "fffffff23cd0b55b"}
     print(json_request)
-    r = requests.post('http://dpow.nanocenter.org/service/', json = json_request)
+    r = requests.post('https://dpow.nanocenter.org/service/', json = json_request)
     complete_time = time.time()
     print(r.text + "\nTook: " + str(complete_time - start_time))
