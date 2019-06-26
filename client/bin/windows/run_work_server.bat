5c6fc7ffe1f9bde709f9ba3192241942e88bd404
keeri <keerifox@users.noreply.github.com>
2019-06-26 09:37:38 +0000

    Add windows batch file for running client & PoW service (#18)
    * Add windows batch file for running client & PoW service
    
    * Delete redundant run_work_server.bat

--- client/bin/windows/run_work_server.bat
+++
@@ -1,2 +0,0 @@
-nano-work-server.exe --gpu 1:0 -l 127.0.0.1:7000
-pause
---
+++ client/run_windows.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: Nano payout address
+set payout_address="nano_1dpowzkw9u6annz4z48aixw6oegeqicpozaajtcnjom3tqa3nwrkgsk6twj7"
+
+:: Desired work type, options are "ondemand", "precache", "any" (default)
+set desired_work_type="any"
+
+:: Optional delay before starting a DPoW client
+set start_delay_seconds=3
+
+echo Starting PoW Service minimized...
+start /min "PoW Service" cmd /c ".\bin\windows\nano-work-server.exe --gpu 1:0 -l 127.0.0.1:7000 && pause"
+
+echo PoW Service started.
+timeout %start_delay_seconds%
+
+echo.
+echo Starting DPoW Client...
+python dpow_client.py --payout %payout_address% --work %desired_work_type%
+
+pause
