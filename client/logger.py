e35bf11262a57f089f4ee1278d4f807ecb11c64d
keeri <keerifox@users.noreply.github.com>
2019-07-02 10:55:12 +0000

    Fix logger rollover breaking logging on Windows (#21)

--- client/logger.py
+++ client/logger.py
@@ -2,6 +2,16 @@ import sys
 import logging
 from logging.handlers import WatchedFileHandler, TimedRotatingFileHandler
 
+class WatchedTimedRotatingFileHandler(TimedRotatingFileHandler, WatchedFileHandler):
+    def __init__(self, filename, **kwargs):
+        super().__init__(filename, **kwargs)
+        self.dev, self.ino = -1, -1
+        self._statstream()
+
+    def emit(self, record):
+        self.reopenIfNeeded()
+        super().emit(record)
+
 def get_logger():
     log_file = "logs/dpow.txt"
     logger = logging.getLogger("dpow")
@@ -10,9 +20,8 @@ def get_logger():
     stream.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s", "%H:%M:%S"))
     stream.setLevel(logging.INFO)
     logger.addHandler(stream)
-    file = WatchedFileHandler(log_file)
-    file.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s", "%Y-%m-%d %H:%M:%S %z"))
+    file = WatchedTimedRotatingFileHandler(log_file, when="d", interval=1, backupCount=30)
+    file.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s\n%(message)s", "%Y-%m-%d %H:%M:%S %z"))
     file.setLevel(logging.DEBUG)
     logger.addHandler(file)
-    logger.addHandler(TimedRotatingFileHandler(log_file, when="d", interval=1, backupCount=30))
     return logger
