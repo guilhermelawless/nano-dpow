d21eee222eaf7d42a41128443b06dcfb639d6f9c
Brandon Berhent <bbedward@gmail.com>
2019-09-17 09:52:43 -0400

    Update default client configuration

--- client/config_parse.py
+++ client/config_parse.py
@@ -3,10 +3,10 @@ import re
 
 WORK_TYPES = ["ondemand", "precache", "any"]
 
-def nano_public_address(string):
-    p = re.compile('^(nano|xrb)_[13]{1}[13456789abcdefghijkmnopqrstuwxyz]{59}$')
+def banano_public_address(string):
+    p = re.compile('^(ban)_[13]{1}[13456789abcdefghijkmnopqrstuwxyz]{59}$')
     if not p.match(string):
-        msg = "%r is not a valid Nano address" % string
+        msg = "%r is not a valid BANANO address" % string
         raise argparse.ArgumentTypeError(msg)
     return string
 
@@ -14,9 +14,9 @@ class DpowClientConfig(object):
 
     def __init__(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--server', type=str, default='mqtt://client:client@dpow.nanocenter.org:1883', help="MQTT broker URI")
+        parser.add_argument('--server', type=str, default='mqtt://client:client@dpow.banano.cc:1883', help="MQTT broker URI")
         parser.add_argument('--worker_uri', type=str, default='127.0.0.1:7000', help='URI of work server listening for RPC calls.')
-        parser.add_argument('--payout', type=nano_public_address, required=True, help='Payout address.')
+        parser.add_argument('--payout', type=banano_public_address, required=True, help='Payout address.')
         parser.add_argument('--work', type=str, action='store', choices=WORK_TYPES, default="any", help='Desired work type. Options: any (default), ondemand, precache.')
 
         args = parser.parse_args()
