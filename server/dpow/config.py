4b2d7d20ba43e50dd5796659706a1ef90af8253a
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2021-03-10 07:56:02 +0000

    Update expiry defaults to more reasonable values

--- server/dpow/config.py
+++ server/dpow/config.py
@@ -11,12 +11,12 @@ class DpowConfig(object):
         parser.add_argument('--mqtt_uri', type=str, default='', help="MQTT broker URI, alternatively set the MQTT_SECRET_URI environment variable, example mqtt://USER:PASSWORD@localhost:1883")
         parser.add_argument('--debug', action='store_true', help="Enable debugging mode (all blocks are precached)")
 
-        parser.add_argument('--block_expiry', type=int, default=4*30*24*60*60, help='How long to keep block hashes in the database. In seconds, default 4 months')
-        parser.add_argument('--account_expiry', type=int, default=365*24*60*60, help='How long to keep accounts in the database, reset when the account frontier is updated. In seconds, default 1 year')
+        parser.add_argument('--block_expiry', type=int, default=24*60*60, help='How long to keep block hashes in the database. In seconds, default 1 day')
+        parser.add_argument('--account_expiry', type=int, default=30*24*60*60, help='How long to keep accounts in the database, reset when the account frontier is updated. In seconds, default 1 month')
         parser.add_argument('--max_multiplier', type=float, default=5.0, help='The maximum multiplier from base difficulty that is accepted. Default 5.0')
-        parser.add_argument('--throttle', type=float, default=10.0, help='The number of requests each service can do every second. Minimum 0.1, default 10.0')
+        parser.add_argument('--throttle', type=float, default=1.0, help='The number of requests each service can do every second. Minimum 0.1, default 1.0')
 
-        parser.add_argument('--difficulty', type=str, default='', help='Difficulty threshold, example fffffc0000000000 . By default, the Nano main network threshold is used')
+        parser.add_argument('--difficulty', type=str, default='', help='Difficulty threshold, example fffffff800000000 . By default, the Nano main network threshold is used')
         args = parser.parse_args()
 
         self.web_path = args.web_path
